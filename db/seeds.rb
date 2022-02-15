388b97b2f647368d1fe0d66775f6b8876cf17b16
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-15 15:44:37 +0100

    Load default seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Message.create!(greetings: 'Good Morning')
+Message.create!(greetings: 'Good Afternoon')
+Message.create!(greetings: 'Good Evening')
+Message.create!(greetings: 'Ndewo')
+Message.create!(greetings: 'Inakwana')
